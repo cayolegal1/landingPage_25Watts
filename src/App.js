f4d605cec0972330b6ceefda4af3c6019ae3e8e3
cayol1 <cayolegal@hotmail.com>
2021-05-02 19:59:48 -0400

    test de ga

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.scss';
+import {useEffect} from 'react';
+import ReactGa from 'react-ga';
 import Header from './componentes/Header/Header';
 import Hero from './componentes/Hero/Hero';
 import About from './componentes/About/About';
@@ -9,6 +11,15 @@ import Footer from './componentes/Footer/Footer';
 
 
 const App =() => {
+
+  useEffect( 
+    () => {
+      ReactGa.initialize("G-QLR6PC1TVJ")
+
+      ReactGa.pageview('/')
+    }
+  )
+
   return (
     <div className="App">
       <Header />
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import ReactGA from 'react-ga';
 
-const TRACKING_ID = "270953556";
 
-ReactGA.initialize(TRACKING_ID);
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
